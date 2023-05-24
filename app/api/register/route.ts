d06d6b272d3bba052c724de5829c9a6d31e5d871
wonkyum-kim <dodo12120@naver.com>
2023-05-24 17:48:34 +0900

    feat: regitser route handler 추가

---
+++ app/api/register/route.ts
@@ -0,0 +1,32 @@
+import bcrypt from 'bcrypt';
+import prisma from '@/app/libs/prismadb';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { email, name, password } = body;
+
+    if (!email || !name || !password) {
+      return new NextResponse('Missing info', { status: 400 });
+    }
+
+    // Hashing by bcrypt
+    const saltRounds = 12;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error: any) {
+    console.log(error, 'REGISTERATION_ERROR');
+    // 500: Internal Serer Error
+    return new NextResponse('Interanl Error', { status: 500 });
+  }
+}
