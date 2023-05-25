ff4649bdd8c191574d2ebeee0bd7491aa9d35104
wonkyum-kim <dodo12120@naver.com>
2023-05-25 15:38:43 +0900

    feat: 프로필 사진 추가

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { User } from '@prisma/client';
+import Image from 'next/image';
+
+interface AvatarProps {
+  user?: User | null;
+}
+
+export default function Avatar({ user }: AvatarProps) {
+  return (
+    <div className='relative'>
+      <div
+        className='
+        relative 
+        inline-block 
+        rounded-full 
+        overflow-hidden
+        h-9 
+        w-9 
+        md:h-11 
+        md:w-11
+      '
+      >
+        <Image
+          fill={true}
+          src={user?.image || '/images/placeholder.jpg'}
+          alt='Avatar'
+        />
+      </div>
+      {/* TODO: user의 상태가 active일 때만 적용하기 */}
+      <span
+        className='
+            absolute 
+            block 
+            rounded-full 
+          bg-green-500 
+            ring-2 
+          ring-white 
+            top-0 
+            right-0
+            h-2 
+            w-2 
+            md:h-3 
+            md:w-3
+          '
+      />
+    </div>
+  );
+}
--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -2,9 +2,17 @@
 
 import useRoutes from '@/app/hooks/useRoutes';
 import DesktopItem from './DesktopItem';
+import { User } from '@prisma/client';
+import { useState } from 'react';
+import Avatar from '../Avatar';
 
-export default function DesktopSidebar() {
+interface DesktopSidebarProps {
+  currentUser: User | null;
+}
+
+export default function DesktopSidebar({ currentUser }: DesktopSidebarProps) {
   const routes = useRoutes();
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div
       className='
@@ -21,21 +29,32 @@ export default function DesktopSidebar() {
         lg:flex
         lg:flex-col
         lg:justify-between
+        lg:gap-y-4
         xl:px-6
       '
     >
-      <ul role='list' className='flex flex-col items-center space-y-1'>
-        {routes.map((item) => (
-          <DesktopItem
-            key={item.label}
-            href={item.href}
-            label={item.label}
-            icon={item.icon}
-            active={item.active}
-            onClick={item.onClick}
-          />
-        ))}
-      </ul>
+      <nav className='flex flex-col justify-between'>
+        <ul role='list' className='flex flex-col items-center space-y-1'>
+          {routes.map((item) => (
+            <DesktopItem
+              key={item.label}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+              active={item.active}
+              onClick={item.onClick}
+            />
+          ))}
+        </ul>
+      </nav>
+      <nav className='flex flex-col justify-between items-center'>
+        <div
+          onClick={() => setIsOpen(true)}
+          className='cursor-pointer hover:opacity-75 transition'
+        >
+          <Avatar user={currentUser} />
+        </div>
+      </nav>
     </div>
   );
 }
