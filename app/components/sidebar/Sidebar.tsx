24c54cfc466d35fc0c31fbf1fdf9005fd39aa42b
wonkyum-kim <dodo12120@naver.com>
2023-05-27 23:12:50 +0900

    fix: type error

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -11,7 +11,7 @@ export default async function Sidebar({
 
   return (
     <div className='h-full'>
-      <DesktopSidebar currentUser={currentUser} />
+      <DesktopSidebar currentUser={currentUser!} />
       <MobileFooter />
       <main className='lg:pl-20 h-full'>{children}</main>
     </div>
