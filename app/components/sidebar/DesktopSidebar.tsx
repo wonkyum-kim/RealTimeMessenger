9fd29ce6307868ebff1102209f3752b446c90767
wonkyum-kim <dodo12120@naver.com>
2023-05-27 14:17:39 +0900

    feat: profile 변경 기능 추가
    - 이름 변경
    - 프로필 사진 변경

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -2,20 +2,45 @@
 
 import useRoutes from '@/app/hooks/useRoutes';
 import DesktopItem from './DesktopItem';
+import Avatar from '../Avatar';
+import SettingsModal from './SettingsModal';
 import { User } from '@prisma/client';
 import { useState } from 'react';
-import Avatar from '../Avatar';
 
 interface DesktopSidebarProps {
-  currentUser: User | null;
+  currentUser: User;
 }
 
 export default function DesktopSidebar({ currentUser }: DesktopSidebarProps) {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div
-      className='
+    <>
+      <SettingsModal
+        currentUser={currentUser}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
+      <div
+        className='
+        hidden 
+        lg:fixed 
+        lg:inset-y-0 
+        lg:left-0 
+        lg:z-40 
+        lg:w-20 
+        xl:px-6
+        lg:overflow-y-auto 
+        lg:bg-white 
+        lg:border-r-[1px]
+        lg:pb-4
+        lg:flex
+        lg:flex-col
+        justify-between
+      '
+      ></div>
+      <div
+        className='
         hidden 
         lg:fixed 
         lg:inset-y-0 
@@ -32,29 +57,30 @@ export default function DesktopSidebar({ currentUser }: DesktopSidebarProps) {
         lg:gap-y-4
         xl:px-6
       '
-    >
-      <nav className='flex flex-col justify-between'>
-        <ul role='list' className='flex flex-col items-center gap-y-1'>
-          {routes.map((item) => (
-            <DesktopItem
-              key={item.label}
-              href={item.href}
-              label={item.label}
-              icon={item.icon}
-              active={item.active}
-              onClick={item.onClick}
-            />
-          ))}
-        </ul>
-      </nav>
-      <nav className='flex flex-col justify-between items-center'>
-        <div
-          onClick={() => setIsOpen(true)}
-          className='cursor-pointer hover:opacity-75 transition'
-        >
-          <Avatar user={currentUser} />
-        </div>
-      </nav>
-    </div>
+      >
+        <nav className='flex flex-col justify-between'>
+          <ul role='list' className='flex flex-col items-center gap-y-1'>
+            {routes.map((item) => (
+              <DesktopItem
+                key={item.label}
+                href={item.href}
+                label={item.label}
+                icon={item.icon}
+                active={item.active}
+                onClick={item.onClick}
+              />
+            ))}
+          </ul>
+        </nav>
+        <nav className='flex flex-col justify-between items-center'>
+          <div
+            onClick={() => setIsOpen(true)}
+            className='cursor-pointer hover:opacity-75 transition'
+          >
+            <Avatar user={currentUser} />
+          </div>
+        </nav>
+      </div>
+    </>
   );
 }
